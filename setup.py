390968270005a1692ba11f928cad3bc64e158e62
Aryan <aryankrsingh004@gmail.com>
2025-08-26 15:49:33 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="soccer-season-predictor",
+    name="football-season-predictor",
     version="0.1.0",
-    description="A package to predict LaLiga season outcomes using a Random Forest model.",
+    description="A package to predict LaLiga and EPL season outcomes using a Random Forest model.",
     author="Aryan Singh",
     packages=find_packages(),
     install_requires=[
